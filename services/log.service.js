ff4df218f08cb53493184ab34d2a8e26db3267fc
marynrak <marynrak@gmail.com>
2022-06-17 09:53:33 +0300

    Adding api services OpenWeather

--- services/log.service.js
+++ services/log.service.js
@@ -1,11 +1,11 @@
 import chalk from 'chalk'
 
-const printError = () => {
-    console.log(chalk.bgRed(' ERROR ') + error())
+const printError = (message) => {
+    console.log(chalk.bgRed(' ERROR ') + '\n' + '-> ' + message)
 }
 
 const printSuccess = (message) => {
-    console.log(chalk.bgGreen(' SUCCESS ') + error())
+    console.log(chalk.bgGreen(' SUCCESS ') + '\n' + '-> ' + message)
 } 
 
 const printHelp = () => {
@@ -13,7 +13,7 @@ const printHelp = () => {
         `${chalk.bgCyan('HELP')}
         -c [CITY]    --- Setup city
         -h           --- For help print
-        -t [API_KEY] --- Save token 
+        -t [TOKEN_API_KEY] --- Save token 
         `
     )
 }
--- weather.js
+++ weather.js
@@ -1,14 +1,35 @@
 #!/usr/bin/env/ node
 import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log.service.js'
+import { getWeather } from './services/api.service.js'
+import { printHelp, printError, printSuccess } from './services/log.service.js'
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
 
+const saveToken = async (token) => {
+    if(!token.length){
+        printError('Token not taken')
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token)
+        printSuccess('Token saved !')
+    } catch (err) {
+        printError(err.message)
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv)
-    console.log(args)
-    if(args.h) {
+    if (args.h) {
         printHelp()
     }
+    if (args.c) {
+
+        
+    }
+    if (args.t) {
+        return saveToken(args.t)
+    }
+
+    getWeather('kyiv')
 }
 
 initCLI()
